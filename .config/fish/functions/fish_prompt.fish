91443b4607c3171d8d06a943f936a0a352337894
miwa <miwa@iMaw.local>
2017-12-30 20:40:09 +0900

    add fish prompt setting

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,6 @@
+function fish_prompt --description 'Write out the prompt'
+    set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
+    set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
+    set prompt_symbol '$'
+    printf "\n%s%s%s %s " (set_color $fish_color_cwd) $pwd (set_color normal) $prompt_symbol
+end
